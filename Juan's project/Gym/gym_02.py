f69af6d2cd9aa0efb199ce5e5db5ce38b8441837
JuanNavarroWSU <20021160@student.westernsydney.edu.au>
2021-07-21 10:44:30 +0800

    Adding q-learning agent

--- Juan's project/Gym/gym_02.py
+++ Juan's project/Gym/gym_02.py
@@ -1,4 +1,9 @@
 import gym
+import numpy as np
+import random
+from IPython.display import clear_output
+
+
 
 env = gym.make("Taxi-v3").env
 
@@ -23,10 +28,10 @@ penalties, reward = 0,0
 
 frames = []
 
-done = False
+#done = False
 
-while not done:
-    action = env.action_space.sample()
+#while not done:
+"""     action = env.action_space.sample()
     state, reward, done, info = env.step(action)
     if reward == -10:
         penalties += 1
@@ -39,4 +44,71 @@ while not done:
     })
     epoch += 1
 print ("Timesteps taken: {}".format(epoch))
-print("Penalties: {}".format(penalties))
+print("Penalties: {}".format(penalties)) """
+
+q_table = np.zeros([env.observation_space.n,env.action_space.n])
+
+## Q_training
+
+alpha = 0.1
+gamma = 0.6
+epsilon = 0.1
+
+#For plotting
+all_epochs = []
+all_penalties = []
+
+for i in range(1,100001):
+    state = env.reset()
+
+    epochs, penalties, reward, = 0,0,0
+    done = False
+    while not done:
+        if random.uniform(0,1) < epsilon:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(q_table[state])
+        
+        next_state, reward, done, info = env.step(action)
+
+        old_value = q_table[state,action]
+        next_max = np.max(q_table[next_state])
+
+        new_value = (1-alpha) * old_value + alpha *(reward+gamma*next_max)
+        q_table[state,action] = new_value
+
+        if reward == -10:
+            penalties+=1
+        state = next_state
+        epochs+=1
+    if i % 1000 ==0:
+        clear_output(wait=True)
+        print(f"Episode {i}")
+print("Training finished.\n")
+
+print(q_table[328])
+
+total_epochs, total_penalties = 0,0
+episodes = 100
+
+for _ in range(episodes):
+    state = env.reset()
+    epochs, penalties,reward = 0,0,0
+    
+    done = False
+
+    while not done:
+        action = np.argmax(q_table[state])
+        state,reward,done, info = env.step(action)
+
+        if reward == -10:
+            penalties+=1
+        
+        epochs+=1
+
+    total_penalties +=penalties
+    total_epochs += epochs
+
+print(f"Results after {episodes} episodes:")
+print(f"Average timesteps per episode: {total_epochs/episodes}")
+print(f"Average penalties per episode: {total_penalties/episodes}")
