ebfa23b79045205f6e6878f3c5fe1c6162100175
JuanNavarro <20021160@student.westernsydney.edu.au>
2021-08-31 18:49:38 +0800

    Using DDPG and DQN agents to train reacher

--- Juan's project/Gym/gym_scara2_training.py
+++ Juan's project/Gym/gym_scara2_training.py
@@ -44,8 +44,8 @@ episodes = 10
 def build_model(states, actions):
     model = Sequential()
     model.add(Flatten(input_shape=(1,states)))
-    model.add(Dense(24,activation="relu"))
-    model.add(Dense(24,activation="relu"))
+    model.add(Dense(48,activation="relu"))
+    model.add(Dense(48,activation="relu"))
     model.add(Dense(actions,activation="linear"))
     return model
 
@@ -58,17 +58,20 @@ def build_agent(model,actions):
 
 
 
+
+print(env.reward_range[0])
+
 model = build_model(states,actions)
 
-dqn = build_agent(model, actions)
-dqn.compile(Adam(lr = 1e-3),metrics =['mae'])
-dqn.fit(env,nb_steps=100000,visualize=False,verbose=1)
+#dqn = build_agent(model, actions)
+#dqn.compile(Adam(lr = 1e-3),metrics =['mae'])
+#dqn.fit(env,nb_steps=100000,visualize=False,verbose=1)
 
-scores = dqn.test(env, nb_episodes = 100, visualize = False)
-print(np.mean(scores.history['episode_reward']))
+#scores = dqn.test(env, nb_episodes = 100, visualize = False)
+#print(np.mean(scores.history['episode_reward']))
 
 
-print(model.summary())
+#print(model.summary())
 
 #dqn._weights('dqn_weights.h5f',overwrite=True) # This is to save the weights of the agent
 # To rebuild the model
