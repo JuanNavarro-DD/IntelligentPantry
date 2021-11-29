d4afbec54015de43aa22708708c6cc6cc03da055
JuanNavarro <20021160@student.westernsydney.edu.au>
2021-11-29 08:39:56 +0800

    final commit

--- Juan's project/Arduino_motor/CNC_Shield_Demo/CNC_Shield_Demo.ino
+++ Juan's project/Arduino_motor/CNC_Shield_Demo/CNC_Shield_Demo.ino
@@ -11,6 +11,8 @@ const int StepZ = 4;
 const int DirZ = 7;
 const int StepA = 12;
 const int DirA = 13;
+const int limitSwitch2 = 10;
+const int limitSwitch3 = 9;
 
 
 
@@ -23,52 +25,64 @@ void setup() {
   pinMode( DirZ,OUTPUT);
   pinMode(StepA,OUTPUT);
   pinMode( DirA,OUTPUT);
+  pinMode(limitSwitch2, INPUT_PULLUP);
 
 }
 
 void loop() {
- digitalWrite(DirX, HIGH);// set direction, HIGH for clockwise, LOW for anticlockwise
+ digitalWrite(DirX, LOW);// set direction, HIGH for clockwise, LOW for anticlockwise
  digitalWrite(DirY, HIGH);
  digitalWrite(DirZ, HIGH);
  digitalWrite(DirA, HIGH);
- 
+// 
 // digitalWrite(StepA,HIGH);
 // digitalWrite(StepA,LOW);
-// digitalWrite(StepX,HIGH);
-// digitalWrite(StepX,LOW);
-// digitalWrite(StepY,HIGH);
-// digitalWrite(StepY,LOW);
-// digitalWrite(StepZ,HIGH);
-// digitalWrite(StepZ,LOW);
+ digitalWrite(StepX,HIGH);
+ digitalWrite(StepX,LOW);
+ digitalWrite(StepY,HIGH);
+ digitalWrite(StepY,LOW);
+ digitalWrite(StepZ,HIGH);
+ digitalWrite(StepZ,LOW);
 
-  for(int x = 0; x<200; x++) { // loop for 200 steps
-   digitalWrite(StepA,HIGH);
-   
-   //delayMicroseconds(5000);
-   digitalWrite(StepA,LOW);
-   delay(1);
-   //delayMicroseconds(2000);
-  }
- delay(1);
- for(int x = 0; x<200; x++) { // loop for 200 steps
+//  for(int x = 0; x<200; x++) { // loop for 200 steps
+//   digitalWrite(StepA,HIGH);
+//   
+//   delayMicroseconds(5000);
+//   digitalWrite(StepA,LOW);
+//   delay(1);
+//   delayMicroseconds(2000);
+//  }
+// delay(1);
+ for(int x = 0; x<500; x++) { // loop for 200 steps
   digitalWrite(StepX,HIGH);
-  
   digitalWrite(StepX,LOW);
-  delayMicroseconds(1000);
+  delayMicroseconds(6000);
  }
  delay(1);
- for(int x = 0; x<200; x++) {
-  digitalWrite(StepY,HIGH);
-  digitalWrite(StepY,LOW);
-  delayMicroseconds(1000);
+ for(int x = 0; x<500; x++) {
+  if (digitalRead(limitSwitch2) !=1) {
+    digitalWrite(StepY,HIGH);
+    digitalWrite(StepY,LOW);
+    delayMicroseconds(6000); 
+  }
  }
  delay(1);
- for(int x = 0; x<200; x++) {
-  digitalWrite(StepZ,HIGH);
-  digitalWrite(StepZ,LOW); 
-  delayMicroseconds(1000);
+ for(int x = 0; x<500; x++) {
+  if(digitalRead(limitSwitch3) !=1){
+    digitalWrite(StepZ,HIGH);
+    digitalWrite(StepZ,LOW); 
+    delayMicroseconds(4000); 
+  }
  }
-delay(1); // delay for 1 second
+ delay(20); // delay for 1 second
+ for(int x = 0; x<400; x++) {
+  if(digitalRead(limitSwitch3) !=1){
+    digitalWrite(StepX,LOW);
+    digitalWrite(StepY,LOW);
+    digitalWrite(StepZ,LOW); 
+    delayMicroseconds(4000); 
+  }
+ }
 
 
 
